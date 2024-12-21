e36977f20c416d549662c6a8a72a52bf69ccbd5b
Abdul Rahiman <160501690+AbdulRahiman01@users.noreply.github.com>
2024-12-21 17:35:33 +0530

    Add files via upload

---
+++ CollegeManagement/src/main/java/com/softgv/cda/service/UserService.java
@@ -0,0 +1,22 @@
+package com.softgv.cda.service;
+
+import org.springframework.http.ResponseEntity;
+
+import com.softgv.cda.entity.User;
+import com.softgv.cda.util.AuthUser;
+
+public interface UserService {
+
+	ResponseEntity<?> findByUsernameAndPassword(AuthUser authUser);
+
+	ResponseEntity<?> saveUser(User user);
+
+	ResponseEntity<?> findAllUsers();
+
+	ResponseEntity<?> findUserById(int id);
+
+	ResponseEntity<?> verifyOTP(int id, int otp);
+
+	ResponseEntity<?> findUserByEmail(String email);
+	
+}
